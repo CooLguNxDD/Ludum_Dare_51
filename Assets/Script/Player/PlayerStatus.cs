4fd13632acb0bdb8d7452bf7e56173c990703c59
Andrew <andrewliang0123@gmail.com>
2022-10-01 19:24:18 -0700

    player Hp bar

--- Assets/Script/Player/PlayerStatus.cs
+++ Assets/Script/Player/PlayerStatus.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStatus : MonoBehaviour
 {
     public int HP = 100;
+    private float animationHP;
+
+    public GameObject playerHPBar;
+    public GameObject playerHPBarInner;
 
     public int GetHP() { return HP; }
+
+    private float value;
+    private float currentDamage;
 
     public void TakeDamage(int damage)
     {
         HP -= damage;
-        Debug.Log(HP);
+        currentDamage = damage;
+        StartCoroutine(takeDamageAnimation());
     }
-    
+
+    public void Update()
+    {
+        if (HP < animationHP)
+        {
+            value = currentDamage * Time.deltaTime * 10;
+            animationHP -= value;
+            playerHPBar.GetComponent<PlayerHpBar>().SetCurrentHealth(animationHP);
+        }
+    }
+    IEnumerator takeDamageAnimation() {
+        playerHPBarInner.GetComponent<Image>().color = Color.red;
+        yield return new WaitForSeconds(0.25f);
+        playerHPBarInner.GetComponent<Image>().color = Color.green;
+    }
+    public void Start()
+    {
+        animationHP = HP;
+        playerHPBar.GetComponent<PlayerHpBar>().SetMaxHealth(HP);
+        playerHPBar.GetComponent<PlayerHpBar>().SetHPBarAlpha(0.8f);
+        
+    }
+
+
+
 
 }
